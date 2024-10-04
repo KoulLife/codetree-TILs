b582738429aa84dc55a4d3554a769004fb1be5a7
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-04 09:35:08 +0900

    Title: 변수 출력하기 2, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 241004/변수 출력하기 2/outputing-variables-2.java
+++ 241004/변수 출력하기 2/outputing-variables-2.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 3;
         char b = 'C';
-        System.out.printf("%s!.....!%d",b,a)
+        System.out.printf("%s!.....!%d",b,a);
     }
 }
