ad84969b3a5343de3f9576a1f96deeb3a01b28fa
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-05 01:23:58 +0900

    Title: 삼항연산자, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241005/삼항연산자/ternary-operator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String out = n == 100 ? "pass" : "failure";
+        System.out.println(out);
+    }
+}
