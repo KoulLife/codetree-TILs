be7d0dda3a15a64923c0cfdbca913116d056ffb3
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-06 01:19:53 +0900

    Title: 원소 10개의 합, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 241006/원소 10개의 합/sum-of-10-elements.java
+++ 241006/원소 10개의 합/sum-of-10-elements.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = int[10];
+        int[] arr = new int[10];
         int res = 0;
 
         for(int i = 0; i < 10; i++){
