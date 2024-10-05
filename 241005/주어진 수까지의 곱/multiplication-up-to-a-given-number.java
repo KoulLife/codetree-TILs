673aa894b7af0858e63104411ed5e821cfd34b94
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-05 21:37:19 +0900

    Title: 주어진 수까지의 곱, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241005/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
+++ 241005/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int a = sc.nextIn();
-        int b = sc.nextIn();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int prod = 1;
 
         for(int i = a; i <= b; i++) {
             prod *= i;
         }
-        
+
         System.out.println(prod);
     }
 }
