6877f1564fefedf3c66fdeaf591d559ef19690db
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-06 01:19:19 +0900

    Title: 원소 10개의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241006/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = int[10];
+        int res = 0;
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < 10; i++){
+            res += arr[i];
+        }
+
+        System.out.println(res);
+    }
+}
