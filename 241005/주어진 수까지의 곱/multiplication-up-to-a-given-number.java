975de084af8e71bbbe2d4700e4fa00ea75116dee
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-05 21:36:23 +0900

    Title: 주어진 수까지의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241005/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextIn();
+        int b = sc.nextIn();
+        int prod = 1;
+
+        for(int i = a; i <= b; i++) {
+            prod *= i;
+        }
+        System.out.println(prod);
+    }
+}
