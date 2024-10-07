668d4194cdfc31d823f4b225bb04904462cd8fa8
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-07 16:23:57 +0900

    Title: 중앙값 계산 2, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/중앙값 계산 2/get-median-2.java
+++ 241007/중앙값 계산 2/get-median-2.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String arr = "";
+        int[] arr = new int[n];
 
-        for(int i = 0; i < n; i++){
-            char a = sc.next().charAt(0);
-            arr += a;
-            
-            if((i + 1) % 2 != 0) {
-                char[] tmp = arr.toCharArray();
-                Arrays.sort(tmp);
-                System.out.print(tmp[i/2] + " ");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if((i+1) % 2 != 0) {
+                Arrays.sort(arr,0,i + 1);
+                System.out.print(arr[i / 2] + " ");
             }
-            
+
         }
 
     }
+
 }
