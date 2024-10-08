b7cb43a204c827dac86fe8ad90341bfdc13767ed
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-08 14:26:13 +0900

    Title: 최대로 겹치는 구간, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241008/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241008/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -6,13 +6,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int offset = 100;
-        int[] arr = new int[201];
+        int offset = 100; // 음수 좌표를 양수로 변환하기 위한 offset
+        int[] arr = new int[201]; // 범위를 충분히 큰 배열로 설정
 
         for(int i = 0; i < n; i++) {
             int x1 = sc.nextInt() + offset;
             int x2 = sc.nextInt() + offset;
 
+            // 구간의 시작점과 끝점 사이에 1씩 더해줌
             for(int j = x1; j <= x2; j++) {
                 arr[j] += 1;
             }
@@ -20,6 +21,7 @@ public class Main {
 
         int count = 0;
 
+        // 배열에서 값이 2 이상인 구간의 개수 계산
         for(int i = 0; i < 201; i++) {
             if(arr[i] >= 2) {
                 count++;
