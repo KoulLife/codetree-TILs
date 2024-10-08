44a3f51e36ee33b382dec51b500fa7da7b1c288e
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-08 14:38:56 +0900

    Title: 최대로 겹치는 구간, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 241008/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241008/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -18,7 +18,7 @@ public class Main {
         }
 
         for(int i = 0; i < n; i++) {
-            for(int j = a1[i]; j <= a2[i]; j++) {
+            for(int j = a1[i]; j < a2[i]; j++) {
                 arr[j]++;
             }
         }
