27347dd411bdb891bfe7680aa7d70707c79934b2
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-10 11:43:22 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241010/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, -1, 0, 1};
+
+        int nx = 0;
+        int ny = 0;
+        int dirNum = 3;
+
+        String com = sc.next();
+        char[] dir = com.toCharArray();
+
+        for(int i = 0; i < dir.length; i++) {
+            if (dir[i] == 'L'){
+                dirNum -= 1;
+            }
+            else if(dir[i] == 'R'){
+                dirNum += 1;
+            }
+            else if(dir[i] == 'F'){
+                nx += dx[dirNum % 4];
+                ny += dy[dirNum % 4];
+            }
+        }
+        System.out.printf("%d %d", nx,ny);
+
+    }
+}
