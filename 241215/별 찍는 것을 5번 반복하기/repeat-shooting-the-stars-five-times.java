0354af1470a8bff0aa19b252645065ceb283f403
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-12-15 17:02:32 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main{
+
+    public static void printStart(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.println("**********");
+        }
+    }
+
+    public static void main(String[] args) {
+        printStart(5);
+    }
+}
