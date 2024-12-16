872a81d39f721c9ed076d01cc7ff10e538e42441
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-12-16 19:34:22 +0900

    Title: 반복 출력하기 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241216/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    public static void printHelloWorld(int n) {
+        if (n == 0)
+            return;
+        
+        System.out.println("HelloWorld");
+        printHelloWorld(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        printHelloWorld(n);
+    }
+}
