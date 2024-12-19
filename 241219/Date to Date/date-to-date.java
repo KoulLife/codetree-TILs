e6d358b42f2e04ddde0b0bbb59e92510ec578165
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-12-19 20:13:34 +0900

    Title: Date to Date, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241219/Date to Date/date-to-date.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int[] days = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+        int res = 0;
+
+        while(true) {
+            if(m1 == m2 && d1 == d2){
+                break;
+            }
+
+            res ++;
+            d1 ++;
+
+            if(days[m1] < d1) {
+                d1 = 0;
+                m1++;
+            }
+        }
+
+        System.out.println(res);
+
+    }
+}
